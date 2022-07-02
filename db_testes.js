256ae76535ff92aedda27b08bea69ed646000f6c
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-07-02 16:54:22 -0300

    remove url

--- db_testes.js
+++ db_testes.js
@@ -1,7 +1,7 @@
 const mongo = require("mongodb");
 const mongoClient = mongo.MongoClient;
 
-const url = "mongodb+srv://king_of_project:UwXWp7BPdGrY1R4l@cluster0.5bcwwx7.mongodb.net/?retryWrites=true&w=majority";
+const url = "";
 
 const connect = async () => {
     const client = await mongoClient.connect(url, {
